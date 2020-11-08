00b5c27945e59403133f679e31d8320e06e17079
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-11-08 01:20:07 -0800

    mongo

--- presence.py
+++ presence.py
@@ -4,6 +4,7 @@ import config
 client_id = config.client_id
 client = Client(client_id)
 client.start()
-client.set_activity(pid=1, state="Simping4Alvin", details= "Alvin Simp Sesh", small_image="alvin", small_text="alvin small", large_image="alvin", large_text="alvin big", start = time.time(), party_id='What goes here', party_size = [1, 50000], join="JOINHERE")
+client.set_activity(pid=3, state="Watching Words 2020", details= "Rooting for Cloud 9", small_image="alvin", small_text="alvin small", large_image="alvin", large_text="alvin big", start = time.time(), party_id="C9VIEWPARTY", party_size = [1, 50000], join="C9WIN")
 while True:
+
     time.sleep(15)
