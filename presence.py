cc2f007ac6e1e8b0730d6c5ae863e5485c572a6d
Kingsley Szeto <szeto.kingsley@gmail.com>
2020-11-08 02:01:35 -0800

    fixed bug

--- presence.py
+++ presence.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 cluster = MongoClient(f"mongodb+srv://kingszeto:{config.mongo_password}@cluster0.zfror.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = cluster["rootwitch"]
 collection = db["teams"]
-party = collection.find({"_id": "tlwin2020"})[0]
+party = collection.find({"_id": "c9win2020"})[0]
 print(party)
 client_id = config.client_id
 client = Client(client_id)
@@ -26,7 +26,7 @@ while True:
     time.sleep(1)
     a = client.loop.run_until_complete(client.read_output())
     if a['evt'] == 'ACTIVITY_JOIN':
-        party=collection.find({"_id": a['data']['secret']})
+        party=collection.find({"_id": a['data']['secret']})[0]
         client.set_activity(pid=0, state=f"Watching {party['match']}",\
             details= f"Rooting for {party['team']}",\
             small_image=party['small'], small_text=party['match'],\
